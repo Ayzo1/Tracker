87359bc32c5da66b14d10892579d8ee39eb02005
Ayaz <44553238+Ayzo1@users.noreply.github.com>
2022-08-18 12:55:39 +0400

    Initial Commit

---
+++ Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tracker
+//
+//  Created by ayaz on 18.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
